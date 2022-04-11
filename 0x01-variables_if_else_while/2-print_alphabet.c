6ea611ecd49cb71975f7333ca147fb10fbb67844
Ruby <dorothymwende505@gmail>
2022-04-11 16:27:40 -0700

    My commit message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,12 +2,14 @@
 /**
  *main - Entry point
  *
- *Return: Always 0 (Success/correct)
+ *Return: Always 0 (Success/Correct)
  */
 int main(void)
+
 {
 char alphabet;
-for (alphabet = "a" alphabet <= "z"; alphabet++)
+
+for (alphabet = "a"; alphabet <= "z"; alphabet++)
 {
 putchar(alphabet);
 }
